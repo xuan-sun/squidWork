e1080c6a279412e63cbb265eee79e0b0856a915a
Xuan Sun <xuan.sun1@gmail.com>
2017-05-02 14:29:15 -0700

    Wrote up the whole fitting function. Can pull a histogram and fit it with a user-defined TF1.

--- histfitter.cc
+++ histfitter.cc
@@ -43,7 +43,6 @@
 
 // global stuff
 using            namespace std;
-double fullTimeWindow = 10000;	// time window to sample over, in seconds
 
 // Plotting functions.
 void PlotHist(TCanvas *C, int styleIndex, int canvasIndex, TH1D *hPlot, TString title, TString command);
@@ -51,7 +50,6 @@ void PlotGraph(TCanvas *C, int styleIndex, int canvasIndex, TGraph *gPlot, TStri
 void PlotFunc(TCanvas *C, int styleIndex, int canvasIndex, TF1 *fPlot, TString command);
 
 // Useful functions for getting the math done.
-void FillOneEvent(TH1D* h, TRandom3* factor, double normalizer);
 double DetectionRate(double time);
 
 // Used for visualization, keeps the graph on screen.
@@ -63,16 +61,52 @@ TApplication plot_program("FADC_readin",0,0,0,0);
 
 int main(int argc, char *argv[])
 {
+  // initial, and approximate, experimental values for use in fit.
+  double N0 = 3.8e5;            // number of UCNs at t=0 in each 3000 cc cell
+  double Tau_beta = 885;        // beta decay lifetime, in seconds
+  double Tau_3 = 500;           // UCN-Helium3 absorption time, in seconds
+//  double Tau_cell = 2000;     // UCN-wall absorption time, in seconds
+//  double T_m = 1000;          // measurement time, in seconds
+//  double T_f = 1000;          // cold neutron fill time, in seconds
+//  double T_d = 400;           // dead time between cycles
+  double P3 = 0.98;             // Helium3 initial polarization, fraction
+  double Pn = 0.98;             // UCN initial polarization, fraction
+  double Gamma_p = 1.0/20000;   // He3 and UCN depolarization rate, in inverse seconds
+  double epsilon_3 = 0.93;      // detection efficiency for UCN-He3 absorption, fraction
+  double epsilon_beta = 0.5;    // detection efficiency for beta decay, fraction
+  double phi_B = 5;             // other background, in Hertz
+
+  // read in a data file
   TFile fIn("SimData/dataset.root","READ");
 
+  // create a canvas for visualization of histogram + fit function
   TCanvas *C = new TCanvas("canvas", "canvas");
 
   // Ensures the seed is different for randomizing in ROOT.
   TRandom3* engine = new TRandom3(0);
   gRandom->SetSeed(0);
 
+  // save our histogram from the read-in file
   TH1D* hEvts = (TH1D*)fIn.Get("Events");
 
+  // Create our fitting function. Set the seed fit values and give the parameters some names for bookkeeping
+  double seedFrequencyValue = 0.1;
+  TF1* fit = new TF1("rate", "[0]*exp(-[1]*x) + [2]*exp(-[1]*x)*(1 - [3]*cos([4]*x)) + [5]", 0, 10000);
+  fit->SetParName(0, "Decay amplitude");
+  fit->SetParameter(0, N0*(epsilon_beta/Tau_beta));
+  fit->SetParName(1, "Time constant");
+  fit->SetParameter(1, Gamma_p);
+  fit->SetParName(2, "Oscillatory amplitude");
+  fit->SetParameter(2, N0*(epsilon_3/Tau_3));
+  fit->SetParName(3, "Total polarization");
+  fit->SetParameter(3, P3*Pn);
+  fit->SetParName(4, "Frequency");
+  fit->SetParameter(4, seedFrequencyValue);
+  fit->SetParName(5, "Background offset");
+  fit->SetParameter(5, phi_B);
+
+  // fit histogram and plot.
+  hEvts->Fit("rate");
   PlotHist(C, 1, 1, hEvts, "Events", "");
 
   // Save our plot and print it out as a pdf.
@@ -81,54 +115,6 @@ int main(int argc, char *argv[])
   plot_program.Run();
 
   return 0;
-}
-
-void FillOneEvent(TH1D* h, TRandom3* factor, double normalizer)
-{
-  double testProb = 1;
-  double pdfValue = 0;
-
-  double testTime;
-
-  while(testProb > pdfValue)
-  {
-    testTime = fullTimeWindow*(factor->Rndm());	// this is the accept-reject part
-
-    pdfValue = DetectionRate(testTime) / normalizer;
-
-    if(pdfValue > 1)
-    {
-      cout << "WARNING: Monte Carlo pdf value greater than 1. Value is " << pdfValue << endl;
-    }
-
-    testProb = factor->Rndm();
-  }
-
-  h->Fill(testTime);	// After we get a good time signature that passes the accept-reject, save it.
-
-}
-
-double DetectionRate(double time)
-{
-  double N0 = 3.8e5;		// number of UCNs at t=0 in each 3000 cc cell
-  double Tau_beta = 885;	// beta decay lifetime, in seconds
-  double Tau_3 = 500;		// UCN-Helium3 absorption time, in seconds
-//  double Tau_cell = 2000;	// UCN-wall absorption time, in seconds
-//  double T_m = 1000;		// measurement time, in seconds
-//  double T_f = 1000;		// cold neutron fill time, in seconds
-//  double T_d = 400;		// dead time between cycles
-  double P3 = 0.98;		// Helium3 initial polarization, fraction
-  double Pn = 0.98;		// UCN initial polarization, fraction
-  double Gamma_p = 1.0/20000;	// He3 and UCN depolarization rate, in inverse seconds
-  double epsilon_3 = 0.93;	// detection efficiency for UCN-He3 absorption, fraction
-  double epsilon_beta = 0.5;	// detection efficiency for beta decay, fraction
-  double phi_B = 5;		// other background, in Hertz
-
-  double rateAtTime_time = N0*(epsilon_beta/Tau_beta)*exp(-Gamma_p*time)
-			 + N0*(epsilon_3/Tau_3)*exp(-Gamma_p*time)*(1 - P3*Pn*cos(0.1*time))
-			 + phi_B;
-
-  return rateAtTime_time;
 }
 
 void PlotFunc(TCanvas *C, int styleIndex, int canvasIndex, TF1 *fPlot, TString command)
