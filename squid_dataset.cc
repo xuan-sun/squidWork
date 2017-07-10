bdca76b8e730f31218c582311ef76d9f8c11d071
Xuan Sun <xuan.sun1@gmail.com>
2017-07-09 21:33:10 -0700

    Modified the dataset generating code to "randomly" kick the frequency we are sampling. Trying to test how well the FFT can still identify things.

--- squid_dataset.cc
+++ squid_dataset.cc
@@ -10,6 +10,9 @@
 #include         <string.h>
 #include         <time.h>
 #include         <assert.h>
+#include	 <cmath>
+#include	 <complex>
+#include	 <iomanip>
 
 // Pretty much all the ROOT libraries I have ever used.
 #include         <TROOT.h>
@@ -40,16 +43,17 @@
 #include         <TLatex.h>
 #include         <TMatrixD.h>
 #include	 <TRandom3.h>
+#include	 <TRandom.h>
+#include 	 <TVirtualFFT.h>
 
 // global stuff
 using            namespace std;
-double fullTimeWindow = 1000;	// time window to sample over, in seconds
 
 double N0 = 3.8e5;            // number of UCNs at t=0 in each 3000 cc cell
 double Tau_beta = 885;        // beta decay lifetime, in seconds
 double Tau_3 = 500;           // UCN-Helium3 absorption time, in seconds
 double Tau_cell = 2000;       // UCN-wall absorption time, in seconds
-//  double T_m = 1000;          // measurement time, in seconds
+double T_m = 1000;            // measurement time, in seconds
 //  double T_f = 1000;          // cold neutron fill time, in seconds
 //  double T_d = 400;           // dead time between cycles
 double P3 = 0.98;             // Helium3 initial polarization, fraction
@@ -60,16 +64,12 @@ double epsilon_3 = 0.93;      // detection efficiency for UCN-He3 absorption, fr
 double epsilon_beta = 0.5;    // detection efficiency for beta decay, fraction
 double phi_B = 5;             // other background, in Hertz
 
-
+const   complex<double> i(0.0,1.0);
 
 // Plotting functions.
 void PlotHist(TCanvas *C, int styleIndex, int canvasIndex, TH1D *hPlot, TString title, TString command);
-void PlotGraph(TCanvas *C, int styleIndex, int canvasIndex, TGraph *gPlot, TString command);
-void PlotFunc(TCanvas *C, int styleIndex, int canvasIndex, TF1 *fPlot, TString command);
-
-// Useful functions for getting the math done.
-void FillOneEvent(TH1D* h, TRandom3* factor, double normalizer);
-double NoiseFunction(double time);
+void PlotGraph(TCanvas *C, int styleIndex, int canvasIndex, TGraph *gPlot, TString title, TString command);
+void PlotFunc(TCanvas *C, int styleIndex, int canvasIndex, TF1 *fPlot, TString title, TString command);
 
 // Used for visualization, keeps the graph on screen.
 TApplication plot_program("FADC_readin",0,0,0,0);
@@ -82,97 +82,134 @@ int main(int argc, char *argv[])
 {
   TFile fOut("squid_dataset.root","RECREATE");
   // creating canvas for plotting
-  TCanvas *C = new TCanvas("canvas", "canvas");
+  TCanvas *C = new TCanvas("canvas", "canvas", 800, 400);
+  C->Divide(2,1);
 
   // Ensures the seed is different for randomizing in ROOT.
   TRandom3* engine = new TRandom3(0);
   gRandom->SetSeed(0);
 
-  int nBins = fullTimeWindow*100;	// this is 1/100 s time bins (i.e. time windows)
+  cout << "Begin generating the fake SQUID signal..." << endl;
 
-  TH1D* hEvts = new TH1D("Events", "Events", nBins, 0, fullTimeWindow);
+  // begin copying of Chris code.
+  double alpha = 1;	// alpha is a multiplier to T2 or T_m
+			// measurement window alpha*T2 determines sensitivity (standard deviation)
+  double S2N_amplitude = 1;     // strength of signal relative to noise of amplitude 1
 
-  double max = 0;
-  // getting the max value of the sample function in time steps 100x smaller than bin width
-  for(int i = 0; i < nBins*100; i++)
+  double T2m = T_m*1.000;
+
+  double padding = 1e7;         // number of zeros we'll pad on to the end for FFT
+
+  vector < pair < double, double > > timeAndFrequencyOfSQUID;
+  double timeStep = 0.01;
+  double frequency = 10;	// setting first frequency value to 10 Hz
+  for(double t = 0; t < alpha*T_m; t = t + timeStep)
   {
-    double valueAti = NoiseFunction(0.01*i);
-    if(valueAti > max)
+    if(fmod(t, 42) < timeStep)	// at each 42s increment, sample and change our frequency in SQUID function
     {
-      max = valueAti;
+      // with equal probability, change the frequency from previous time "chunk" by 3Hz
+      if(engine->Rndm() > 0.5)
+      {
+        frequency = frequency + 3;
+      }
+      else
+      {
+        frequency = frequency - 3;
+      }
     }
+
+    timeAndFrequencyOfSQUID.push_back(make_pair(t, frequency));
   }
 
-  int maxNbEvents = 1e7;
-  for(int i = 0; i < maxNbEvents; i++)
+  vector <double> noise;
+  vector <double> squid;
+  for(unsigned int j = 0; j < timeAndFrequencyOfSQUID.size(); j++)
   {
-    FillOneEvent(hEvts, engine, max);
+    noise.push_back(engine->Gaus(0,1));
+    squid.push_back(real(S2N_amplitude*exp(-2*M_PI*timeAndFrequencyOfSQUID[j].second*timeAndFrequencyOfSQUID[j].first*i)*exp(-timeAndFrequencyOfSQUID[j].first/T2m)));
   }
 
+  vector <double> total;
+  for(unsigned int j = 0; j < timeAndFrequencyOfSQUID.size(); j++)
+  {
+    total.push_back(noise[j] + squid[j]);
+  }
 
-  PlotHist(C, 1, 1, hEvts, "Sampled Events", "");
+  cout << "Completed generating the dataset..." << endl;
+
+  // this loop below exists so the TGraph* totalSignal can be referenced correctly
+  vector <double> time;
+  for(unsigned int i = 0; i < timeAndFrequencyOfSQUID.size(); i++)
+  {
+    time.push_back(timeAndFrequencyOfSQUID[i].first);
+  }
+
+  TGraph* totalSignal = new TGraph(time.size(), &(time[0]), &(total[0]));
+
+  cout << "Performing FFT..." << endl;
+
+  // do the FFT and get the values we care about.
+  total.resize(total.size() + padding);
+  int fftSize = total.size();
+  TVirtualFFT* fft_total = TVirtualFFT::FFT(1, &fftSize, "R2C P K");
+  fft_total->SetPoints(&(total[0]));
+  fft_total->Transform();
+
+  cout << "Completed FFT..." << endl;
+
+  cout << "Begin displaying results..." << endl;
+
+  TH1* fft_total_hist = 0;
+  fft_total_hist = TH1::TransformHisto(fft_total, fft_total_hist, "Re");
+
+  // the fftSize-1 is for an overflow/underflow bin offset that biases the frequency
+  TH1D* hFFT = new TH1D("Hist FFT", "Histogram of FFT SQUID + noise"
+			, (fftSize - 1) / 2
+			, fft_total_hist->GetXaxis()->GetXmin() / ((fftSize-1)*timeStep)
+			, fft_total_hist->GetXaxis()->GetXmax() / (2.0*(fftSize-1)*timeStep));
+
+  for(int j = 0; j < hFFT->GetNbinsX(); j++)
+  {
+    hFFT->SetBinContent(j, fft_total_hist->GetBinContent(j));
+  }
+  delete fft_total_hist;
+  delete fft_total;
+  total.clear();
+
+  PlotGraph(C, 1, 1, totalSignal, "SQUID time domain signal", "");
+  PlotHist(C, 1, 2, hFFT, "SQUID frequency domain signal", "");
 
   fOut.Write();
 
   // Save our plot and print it out as a pdf.
-  C -> Print("output_squid_dataset.pdf");
+  C -> Print("output_squid_dataset.png");
   cout << "-------------- End of Program ---------------" << endl;
   plot_program.Run();
 
   return 0;
 }
 
-void FillOneEvent(TH1D* h, TRandom3* factor, double normalizer)
-{
-  double testProb = 1;
-  double pdfValue = 0;
-
-  double testTime;
-
-  while(testProb > pdfValue)
-  {
-    testTime = fullTimeWindow*(factor->Rndm());	// this is the accept-reject part
-
-    pdfValue = NoiseFunction(testTime) / normalizer;
-
-    if(pdfValue > 1)
-    {
-      cout << "WARNING: Monte Carlo pdf value greater than 1. Value is " << pdfValue << endl;
-    }
-
-    testProb = factor->Rndm();
-  }
-
-  h->Fill(testTime);	// After we get a good time signature that passes the accept-reject, save it.
-
-}
-
-double NoiseFunction(double time)
-{
-  double rateAtTime_time = N0*(epsilon_beta/Tau_beta)*exp(-Gamma_T*time)
-			 + N0*(epsilon_3/Tau_3)*exp(-Gamma_T*time)*(1 - P3*Pn*cos(2*M_PI*10*time))
-			 + phi_B;
-
-  return rateAtTime_time;
-}
-
-void PlotFunc(TCanvas *C, int styleIndex, int canvasIndex, TF1 *fPlot, TString command)
+void PlotFunc(TCanvas *C, int styleIndex, int canvasIndex, TF1 *fPlot, TString title, TString command)
 {
   C -> cd(canvasIndex);
 
   fPlot->SetLineColor(styleIndex % 50);	// only 50 colors in set line color.
-  fPlot->GetYaxis()->SetRangeUser(-40, 40);
+//  fPlot->GetYaxis()->SetRangeUser(-40, 40);
   fPlot->GetYaxis()->SetTitle("Erecon error");
   fPlot->GetXaxis()->SetTitle("Evis");
+  fPlot->SetTitle(title);
 
   fPlot->Draw(command);
 }
 
-void PlotGraph(TCanvas *C, int styleIndex, int canvasIndex, TGraph *gPlot, TString command)
+void PlotGraph(TCanvas *C, int styleIndex, int canvasIndex, TGraph *gPlot, TString title, TString command)
 {
   C -> cd(canvasIndex);
   gPlot->SetLineColor(styleIndex);
-  gPlot->GetYaxis()->SetRangeUser(-40, 40);
+  gPlot->SetTitle(title);
+  gPlot->GetXaxis()->SetTitle("Time (s)");
+  gPlot->GetYaxis()->SetTitle("Amplitude (arbitrary)");
+//  gPlot->GetYaxis()->SetRangeUser(-40, 40);
   gPlot->Draw(command);
 }
 
@@ -180,9 +217,9 @@ void PlotHist(TCanvas *C, int styleIndex, int canvasIndex, TH1D *hPlot, TString 
 {
   C -> cd(canvasIndex);
   hPlot -> SetTitle(title);
-  hPlot -> GetXaxis() -> SetTitle("Time (s)");
+  hPlot -> GetXaxis() -> SetTitle("Frequency (Hz)");
   hPlot -> GetXaxis() -> CenterTitle();
-  hPlot -> GetYaxis() -> SetTitle("Counts (N)");
+  hPlot -> GetYaxis() -> SetTitle("Amplitude (arbitrary)");
   hPlot -> GetYaxis() -> CenterTitle();
 //  hPlot -> GetYaxis() -> SetRangeUser(0, 0.000004);
 
